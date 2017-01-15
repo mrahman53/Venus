cb254c0a74537642d70b98af4e3cb9087b9aba2c
mrahman <rahmanww@gmail.com>
2017-01-15 13:38:00 -0500

    refactor

--- Android/src/test/java/phone/SnapChatHome.java
+++ Android/src/test/java/phone/SnapChatHome.java
@@ -4,25 +4,17 @@ import common.Base;
 import data.UserInfo;
 import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
+import signUp.Login;
 
 /**
  * Created by mrahman on 9/5/15.
  */
-public class SnapChatHome extends Base {
+public class SnapChatHome extends Login {
 
     UserInfo user = new UserInfo();
     @Test
     public void mainPage()throws InterruptedException{
-        System.out.println("App is launched");
-        clickByXpath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.ScrollView[1]/android.widget.RelativeLayout[1]/android.widget.LinearLayout[1]/android.widget.Button[1]");
-        System.out.println("Click on Log In to enter UserName and Password");
-        typeByXpath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.ScrollView[1]/android.widget.RelativeLayout[1]/android.widget.EditText[1]", user.userName);
-        System.out.println("User Name is entered");
-        typeByXpath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.ScrollView[1]/android.widget.RelativeLayout[1]/android.widget.EditText[2]", user.passWord);
-        System.out.println("Password is entered");
-        clickByXpath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.Button[1]");
-        System.out.println("Click On LogIn button to Authenticate");
-        sleep(3);
+       loginSnapChat();
     }
 
 }
